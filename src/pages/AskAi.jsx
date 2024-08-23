92d835a2996b8773a7164397c63f1456f28b2e72
aditya000099 <adi.agg099@gmail.com>
2024-08-23 06:41:07 +0530

    fixes

--- src/pages/AskAi.jsx
+++ src/pages/AskAi.jsx
@@ -57,102 +57,100 @@ const AskAi = () => {
       alert("Please select a date.");
     }
   };
-  const [hasGpu, setHasGpu] = useState(false);
+//   const [hasGpu, setHasGpu] = useState(false);
 
-  useEffect(() => {
-    setHasGpu(hasWebGL());
-  }, []);
+//   useEffect(() => {
+//     setHasGpu(hasWebGL());
+//   }, []);
 
-  function hasWebGL() {
-    try {
-      const canvas = document.createElement("canvas");
-      return !!(
-        window.WebGLRenderingContext && 
-        (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
-      );
-    } catch (e) {
-      return false;
-    }
-  }
+//   function hasWebGL() {
+//     try {
+//       const canvas = document.createElement("canvas");
+//       return !!(
+//         window.WebGLRenderingContext &&
+//         (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
+//       );
+//     } catch (e) {
+//       return false;
+//     }
+//   }
 
   return (
-    <div className={styles.container}>
-    <div className="bg-black w-full min-h-screen flex flex-col items-center justify-center">
-  <div className="bg-black flex flex-row justify-center items-center gap-40 w-full max-w-6xl p-4">
-    <div className="flex flex-col items-center p-4 justify-center font-black text-5xl">
-    <div>
-      Astro<span className="text-lime-300">Ai</span>
-      </div>
-      <h1 className="text-2xl mt-4 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-pink-500">
-  Get horoscope in seconds
-</h1>
-    </div>
-    
+    <div className="w-[98.5vw] overflow-x-hidden">
+      <div className="bg-black w-full min-h-screen flex flex-col items-center justify-center">
+        <div className="bg-black flex flex-row justify-center items-center gap-40 w-full max-w-6xl p-4">
+          <div className="flex flex-col items-center p-4 justify-center font-black text-5xl">
+            <div>
+              Astro<span className="text-lime-300">Ai</span>
+            </div>
+            <h1 className="text-2xl mt-4 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-pink-500">
+              Get horoscope in seconds
+            </h1>
+          </div>
+        </div>
 
-  </div>
-
-  <div className="flex flex-col items-center justify-center w-full bg-black mt-4">
-    <Spline
-      scene="https://prod.spline.design/9rB92fOsQmke9hQu/scene.splinecode"
-      className="h-4/5 max-h-[30rem] w-full"
-    />
-    <div className="flex flex-col w-full max-w-4xl px-4 py-4 space-y-4 z-10">
-      <div className="w-full flex items-center">
-        <input
-          type="text"
-          value={query}
-          onChange={handleInputChange}
-          onKeyDown={(e) => e.key === "Enter" && handleSubmit(e)}
-          className="rounded-3xl border-white shadow-lg w-full h-16 p-4 font-bold text-white outline-none placeholder-white"
-          placeholder="Ask a question..."
-        />
-        <input
-          type="date"
-          value={date}
-          onChange={handleDateChange}
-          className="rounded-3xl border-white shadow-lg h-16 p-4 font-bold text-white outline-none ml-4 w-1/4"
-        />
-        <button onClick={handleSubmit} className="w-16 h-16 rounded-full bg-lime-300 text-black font-bold text-3xl text-center ml-4">
-          
-        </button>
-      </div>
-      <div className="bg-white bg-opacity-5 rounded-lg p-4 space-y-4 overflow-auto">
-        <div className="space-y-2">
-          {chatHistory.map((chat, index) => (
-            <div
-              key={index}
-              className="flex flex-col space-y-2"
-              ref={index === 0 ? endOfChatRef : null}
-            >
-              <div className="flex items-start">
-                <div className="flex items-center space-x-2">
-                  <div className="w-8 h-8 bg-black border border-white text-white font-bold rounded-full flex items-center justify-center">
-                    Me
+        <div className="flex flex-col items-center justify-center w-full bg-black">
+          <Spline
+            scene="https://prod.spline.design/9rB92fOsQmke9hQu/scene.splinecode"
+            className="h-4/5 max-h-[30rem] w-full"
+          />
+          <div className="flex flex-col w-full max-w-4xl px-4 py-4 space-y-4 z-10">
+            <div className="w-full flex items-center">
+              <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit(e)}
+                className="rounded-3xl border-white shadow-lg w-full h-16 p-4 font-bold text-white outline-none placeholder-white"
+                placeholder="Ask a question..."
+              />
+              <input
+                type="date"
+                value={date}
+                onChange={handleDateChange}
+                className="rounded-3xl border-white shadow-lg h-16 p-4 font-bold text-white outline-none ml-4 w-1/4"
+              />
+              <button
+                onClick={handleSubmit}
+                className="w-16 h-16 rounded-full bg-lime-300 text-black font-bold text-3xl text-center ml-4"
+              ></button>
+            </div>
+            <div className="bg-white bg-opacity-5 rounded-lg p-4 space-y-4 overflow-auto">
+              <div className="space-y-2">
+                {chatHistory.map((chat, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col space-y-2"
+                    ref={index === 0 ? endOfChatRef : null}
+                  >
+                    <div className="flex items-start">
+                      <div className="flex items-center space-x-2">
+                        <div className="w-8 h-8 bg-black border border-white text-white font-bold rounded-full flex items-center justify-center p-5">
+                          Me
+                        </div>
+                        <div className="bg-lime-300 text-black p-3 rounded-2xl max-w-xs font-semibold">
+                          <p>{chat.query}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-end justify-end">
+                      <div className="flex items-center space-x-2">
+                        <div className="w-8 h-8 bg-black text-white border-2 border-purple-800 rounded-full flex items-center justify-center font-bold p-5">
+                          AI
+                        </div>
+                        <div className="bg-black text-white p-3 rounded-2xl max-w-[28rem] font-semibold">
+                          <p>{chat.answer}</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="bg-lime-300 text-black p-3 rounded-2xl max-w-xs font-semibold">
-                    <p>{chat.query}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex items-end justify-end">
-                <div className="flex items-center space-x-2">
-                  <div className="w-8 h-8 bg-black text-white border-2 border-purple-800 rounded-full flex items-center justify-center font-bold">
-                    AI
-                  </div>
-                  <div className="bg-black text-white p-3 rounded-2xl max-w-[28rem] font-semibold">
-                    <p>{chat.answer}</p>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-</div>
-
   );
 };
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,55 +1,128 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import { FaGoogle, FaApple, FaFacebook } from "react-icons/fa";
+import bpat from "../assets/bpat.png";
+import { FaRegHandPointRight } from "react-icons/fa6";
 
 const LoginPage = () => {
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent the form from refreshing the page
-    window.location.href = '/'; // Redirect to '/'
+    window.location.href = "/"; // Redirect to '/'
   };
 
   return (
-    <div className='flex justify-center items-center w-screen'>
-    
-      <div className="w-full h-screen flex flex-col justify-center items-center bg-[#020013]">
-      <h1 className='text-5xl font-bold'>AstroGuide</h1>
-        <div className="w-full max-w-md bg-white rounded-lg p-8 shadow-lg mt-10">
-          <h2 className="text-2xl font-bold text-center text-black mb-6">Login</h2>
-          
-          {/* <form onSubmit={handleSubmit}> */}
-            <div className="mb-4">
-              <label className="block text-black text-sm font-bold mb-2" htmlFor="username">
-                Username
-              </label>
+    <div className="flex justify-center items-center w-screen h-screen bg-black">
+      {/* Image container */}
+      <div className="relative flex justify-center items-center w-full h-full">
+        <img
+          src={bpat}
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+
+        {/* Blue light gradient */}
+        <div className="absolute inset-0 bg-gradient-to-b from-blue-900/30 to-transparent"></div>
+
+        {/* Flex container for forms */}
+        <div className="absolute inset-0 flex items-center justify-center space-x-8 gap-20">
+          {/* Left Login Form */}
+          <div className="flex flex-col justify-center items-center bg-white bg-opacity-0 rounded-2xl p-10 px-20 shadow-lg">
+            <h2 className="text-5xl font-black text-center text-white mb-4">
+              AstroGuide
+            </h2>
+            <h2 className="text-xl font-semibold text-center text-lime-300 mb-4">
+              Your Personal Horoscoper
+            </h2>
+            <div className="mt-12 flex flex-col justify-start items-start">
+              <div className="flex flex-row gap-10 justify-center items-center">
+                <FaRegHandPointRight className="text-lime-300 text-xl" />
+                <h2 className="text-lg  text-center text-white">
+                  Daily Horoscope
+                </h2>
+              </div>
+              <div className="flex flex-row gap-10 justify-center items-center mt-3">
+                <FaRegHandPointRight className="text-lime-300 text-xl" />
+                <h2 className="text-lg  text-center text-white">
+                  Personalised Readings Everyday
+                </h2>
+              </div>
+              <div className="flex flex-row gap-10 justify-center items-center mt-3">
+                <FaRegHandPointRight className="text-lime-300 text-xl " />
+                <h2 className="text-lg  text-center font-semibold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-pink-500">
+                  AI horoscope in seconds
+                </h2>
+              </div>
+            </div>
+
+           
+          </div>
+
+          {/* Right Login Form */}
+          <div className="flex flex-col justify-center items-center bg-white bg-opacity-10 rounded-2xl p-10 px-20 shadow-lg">
+            <h2 className="text-2xl font-bold text-center text-white mb-4">
+              Login
+            </h2>
+            <h2 className="text-sm font-semibold text-center text-white mb-4">
+              Enter your details to login to your account
+            </h2>
+
+            <div className="mb-4 w-full">
               <input
                 type="text"
-                id="username"
-                placeholder="Enter your username"
-                className="w-full px-4 py-3 rounded-lg bg-gray-100 text-black focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Enter Email"
+                className="w-full px-4 py-3 rounded-lg bg-white bg-opacity-10 text-black focus:outline-none focus:bg-white focus:border-gray-500"
               />
             </div>
 
-            <div className="mb-6">
-              <label className="block text-black text-sm font-bold mb-2" htmlFor="password">
-                Password
-              </label>
+            <div className="mb-6 w-full">
               <input
                 type="password"
-                id="password"
-                placeholder="Enter your password"
-                className="w-full px-4 py-3 rounded-lg bg-gray-100 text-black focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Passcode"
+                className="w-full px-4 py-3 rounded-lg bg-white bg-opacity-10 text-black focus:outline-none focus:bg-white focus:border-gray-500"
               />
             </div>
+            <h1 className="mt-4 text-xs font-semibold text-white text-left w-full">
+              Having trouble in sign in?
+            </h1>
 
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between mt-8 w-full">
               <button
                 type="submit"
                 onClick={handleSubmit}
-                className="w-full bg-[#020013] text-white font-bold py-3 px-4 rounded-lg hover:bg-gray-800 focus:outline-none"
+                className="w-full bg-lime-300 text-black font-bold py-3 px-4 rounded-lg hover:bg-lime-400 focus:outline-none"
               >
                 Login
               </button>
             </div>
-          {/* </form> */}
+            <h1 className="mt-6 text-sm font-semibold text-white text-center">
+              Or Sign in with
+            </h1>
+            <div className="mt-6 flex flex-row gap-3 justify-center items-center">
+              <button
+                onClick={() => (window.location.href = "/book")}
+                className="w-1/2 bg-white bg-opacity-10 text-white border-2 font-bold border-gray-900 rounded-lg px-3 py-2  flex flex-row justify-center items-center gap-3"
+              >
+                <FaGoogle />
+                Google
+              </button>
+              <button
+                onClick={() => (window.location.href = "/book")}
+                className="w-1/2 bg-white bg-opacity-10 text-white border-2 font-bold border-gray-900 rounded-lg px-3 py-2  flex flex-row justify-center items-center gap-3"
+              >
+                <FaApple />
+                Apple
+              </button>
+              {/* <button
+                onClick={() => (window.location.href = "/book")}
+                className="bg-white border-2 font-bold border-gray-900 rounded-md px-3 py-2 text-gray-900 flex flex-row justify-center items-center gap-3"
+              >
+                <FaFacebook />
+                Facebook
+              </button> */}
+            </div>
+            <h1 className="mt-8 text-sm font-semibold text-white text-center">
+              Don't have an account? Sign Up now
+            </h1>
+          </div>
         </div>
       </div>
     </div>
